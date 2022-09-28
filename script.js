0980501e353a5cc9296a11f4215edd6491135fad
EdwinSch <edwinschutjes@gmail.com>
2022-09-28 20:54:04 +0200

    toggle logic JS

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 /* ---- TARGETS && INITIALIZERS ---- */
+const toggleBtn = document.querySelectorAll(".toggle")
+
 
 /* ---- FUNCTIONS ---- */
 
-console.log("meh");
+toggleBtn.forEach(btn => {
+    btn.addEventListener('click', function () {
+        // Show/Hide text content
+        this.parentElement.nextElementSibling.classList.toggle("show-text");
+        // Switch Icon
+        this.children[0].classList.toggle("show-chevron-up");
+        this.children[1].classList.toggle("hide-chevron-down");
+    })
+});
